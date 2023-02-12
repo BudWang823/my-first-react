b2548e9f341ebcb5f0c239c9a8e0aec933584306
wangzhen <17600820823@163.com>
2023-02-12 22:58:30 +0800

    函数式组件

--- src/01-base/02-function.js
+++ src/01-base/02-function.js
@@ -1,37 +1,10 @@
-// class Test {
-// 	constructor(props) {
-// 		this.name = props
-// 	}
-//
-// 	test1() {
-// 		console.log(this.name, 'test1')
-// 	}
-//
-// 	test2() {
-// 		console.log(this.name, 'test2')
-//
-// 	}
-//
-// }
-//
-// class Child extends Test {
-//
-// }
-//
-//
-// const obj = new Test('Test')
-// const obj2 = new Child('Child')
-// obj.test1()
-// obj2.test1()
-
-import React from "react";
-
-class App extends React.Component{
-	render() {
-		return <div>
-			第一个react
+function App() {
+	return(
+		<div>
+			asdfadf
 		</div>
-	}
+	)
 
 }
+
 export default App
