59fb657861457ba5f62abd217568aaa9ce4e4147
wangzhen <17600820823@163.com>
2023-02-14 15:51:00 +0800

    受控组件

--- src/01-base/14-受控案例todolist.js
+++ src/01-base/14-受控案例todolist.js
@@ -1,13 +1,71 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 
-class App extends Component {
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			todoList: [],
+			text: ''
+		}
+	}
+	handleAddTodo = () => {
+		const {text} = this.state
+		if (!text) {
+			alert('请输入')
+			return
+		}
+		this.setState({
+			todoList: [...this.state.todoList, {id: `${new Date().valueOf()}`, text,isChecked: false}],
+			text: ''
+		})
+		setTimeout(() => {
+			console.log(this.state.todoList)
+		},1)
+	}
+	handleDelTodo = (item, index) => {
+		const {todoList} = this.state
+		todoList.splice(index,1)
+		this.setState({
+			todoList
+		})
+	}
+	handleChecked = ( index) => {
+		const newlist = [...this.state.todoList]
+		newlist[index].isChecked = !newlist[index].isChecked
+		this.setState({
+			todoList:newlist
+		})
+	}
+
 	render() {
+		const {
+			todoList
+		} = this.state
+		const renderTodoList = todoList.map((item, index) => {
+			return (
+				<li key={item.id}>
+
+					<input type="checkbox" checked={item.isChecked} onChange={(e) => {this.handleChecked(index)}}></input>
+					<span style={{textDecoration: item.isChecked? 'line-through': 'none'}} dangerouslySetInnerHTML={{__html: `${index + 1}: ${item.text}`}}></span>
+					<button onClick={_ => {this.handleDelTodo(item, index)}}>删除</button>
+				</li>
+			)
+		})
 		return (
 			<div>
-
+				<div>
+					<input value={this.state.text} onChange={(e) => {
+						this.setState({
+							text: e.target.value
+						})
+					}} type="text"/>
+					<button onClick={_ => {
+						this.handleAddTodo()
+					}}>add
+					</button>
+				</div>
+				{todoList.length ? <ul>{renderTodoList}</ul> : <div>暂无数据</div>}
 			</div>
-		);
+		)
 	}
 }
-
-export default App;
